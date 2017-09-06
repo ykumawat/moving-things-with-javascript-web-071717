4946543f5769f0406ccfb96332d1e31cf795e57c
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-09-05 20:50:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+const dodger = $('#dodger')
+
+
+$(dodger).on('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
